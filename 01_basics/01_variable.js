7652d377b4456292bc80746feefb17376d8074a1
Zohaib <zohaib>
2025-05-09 17:35:42 +0500

    variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,7 @@
+const accountId =122334
+let accountEmail = "google@gmail.com"
+var accountPassword ="1234"
+accountCity = "peshawar"
+
+
+console.table([accountEmail,accountId,accountPassword,accountCity]);
--- test.js
+++ test.js
@@ -1,13 +1,3 @@
 console.log('test');
 console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
 
