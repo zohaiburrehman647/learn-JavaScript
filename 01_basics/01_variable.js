07dff1d4799dbd14bf39ce0eac2c68b4457aa390
Zohaib <zohaib>
2025-05-13 01:06:12 +0500

    variables and datatypes

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -4,4 +4,8 @@ var accountPassword ="1234"
 accountCity = "peshawar"
 
 
+/* prefer not use var
+because of issue in block scope and functional scope 
+ */
+
 console.table([accountEmail,accountId,accountPassword,accountCity]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+let name = "zohaib"
+let age =50
+let isloggedIn = false
+
+
+/* number => 2 power 53
+
+   Bigint 
+
+   String =>
+      
+   Boolean => true/false 
+
+   Null =>   standalnone valve 
+
+   Undefined => 
+
+    Symbol => unique
+
+*/
+
+/*
+       *** primitive datatypes ***
+
+       7 types 
+        => String , Number , Boolean ,Null, undefined , symbol , bigINt
+        
+
+       *** Non Primitive ***
+
+
+       Array , Objects , Functions
+*/ 
